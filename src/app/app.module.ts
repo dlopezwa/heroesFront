6d3bb64ff11d2f581b4adc6cd9017db1523f2583
David Lopez Watsing <david.lopez.watsing@gmail.com>
2021-03-28 17:35:15 +0200

    Creación de Pestañas y páginas Principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/shared/header/header.component';
 import { MainComponent } from './pages/shared/main/main.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { MissionComponent } from './pages/mission/mission.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    HeroeComponent,
+    MissionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
